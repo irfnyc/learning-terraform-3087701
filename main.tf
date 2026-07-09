46a4fe02b3d51708a6526963d523189bd02c0989
irfnyc <irfordham@icloud.com>
2026-07-09 16:33:25 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "aws_ami" "app_ami" {
   most_recent = true
 
   filter {
-    name   = "name"
+    name   = "AMI name"
     values = ["aws-elasticbeanstalk-amzn-*.64bit_arm-eb_tomcat11corretto25_amazon_linux_2023-hvm-*"]
   }
 
